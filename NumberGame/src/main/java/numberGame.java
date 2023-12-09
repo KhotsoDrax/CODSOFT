36e4ea337810fca32780e9c8d63353c8216d23a9
kmeje023a <kmeje023a@student.wethinkcode.co.za>
2023-12-10 00:21:06 +0200

    number Game prgram without UI

---
+++ NumberGame/src/main/java/numberGame.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class numberGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int numberToGuess = random.nextInt(100) + 1;
+        int attempts = 0;
+        int maxAttempts = 5;
+        int guess;
+        boolean guessedCorrectly = false;
+
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("Try to guess the magic number between 1 and 100.");
+        System.out.println("You have 5 attempts.");
+
+        while (attempts < maxAttempts) {
+            System.out.print("Enter your guess: ");
+            guess = scanner.nextInt();
+            attempts++;
+
+            if (guess < numberToGuess) {
+                System.out.println("Too low! Try again.");
+            } else if (guess > numberToGuess) {
+                System.out.println("Too high! Try again.");
+            } else {
+                guessedCorrectly = true;
+                break;
+            }
+        }
+
+        if (guessedCorrectly) {
+            System.out.println("Congratulations! You've guessed the number " + numberToGuess + " in " + attempts + " attempts.");
+        } else {
+            System.out.println("Sorry, you've run out of attempts. The correct number was: " + numberToGuess);
+        }
+
+        scanner.close();
+    }
+}
---
+++ out/production/CODSOFT/README.md
@@ -0,0 +1,2 @@
+# CODSOFT
+Internship Programme
