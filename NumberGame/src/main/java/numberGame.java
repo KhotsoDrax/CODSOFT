f0ba0c992d05072a93d89de31445ff977008155d
kmeje023a <kmeje023a@student.wethinkcode.co.za>
2023-12-12 05:42:37 +0200

    NumberGame Update Added the difficulty level feature for users to choose from 1-4 for better game experience

--- NumberGame/src/main/java/numberGame.java
+++ NumberGame/src/main/java/numberGame.java
@@ -5,15 +5,52 @@ public class numberGame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
+
+        // Set difficulty levels
+        int easyMaxAttempts = 20;
+        int mediumMaxAttempts = 15;
+        int hardMaxAttempts = 10;
+        int veryHardMaxAttempts = 5;
+
+        // Ask the user to choose a difficulty level
+        System.out.println("Choose a difficulty level:");
+        System.out.println("1. Easy");
+        System.out.println("2. Medium");
+        System.out.println("3. Hard");
+        System.out.println("4. Very Hard");
+        System.out.print("Enter the corresponding number for your choice: ");
+
+        int difficultyChoice = scanner.nextInt();
+        int maxAttempts;
+
+        // Set maxAttempts based on the difficulty level chosen
+        switch (difficultyChoice) {
+            case 1:
+                maxAttempts = easyMaxAttempts;
+                break;
+            case 2:
+                maxAttempts = mediumMaxAttempts;
+                break;
+            case 3:
+                maxAttempts = hardMaxAttempts;
+                break;
+            case 4:
+                maxAttempts = veryHardMaxAttempts;
+                break;
+            default:
+                System.out.println("Invalid choice. Setting difficulty to Medium.");
+                maxAttempts = mediumMaxAttempts;
+                break;
+        }
+
         int numberToGuess = random.nextInt(100) + 1;
         int attempts = 0;
-        int maxAttempts = 15;
         int guess;
         boolean guessedCorrectly = false;
 
         System.out.println("Welcome to the Number Guessing Game!");
-        System.out.println("Try to guess the magic number between 1 and 100.");
-        System.out.println("You have 15 attempts.");
+        System.out.println("Try to guess the randomly selected number between 1 and 100.");
+        System.out.println("You have " + maxAttempts + " attempts.");
 
         while (attempts < maxAttempts) {
             System.out.print("Enter your guess: ");
@@ -27,6 +64,11 @@ public class numberGame {
             } else {
                 guessedCorrectly = true;
                 break;
+            }
+
+            if (attempts < maxAttempts) {
+                // Only ask the user to try again if there are more attempts available
+                System.out.println("You have " + (maxAttempts - attempts) + " attempts remaining.");
             }
         }
 
