c842e46a1f0d0fdadfbef603bc2474fb0eb807e0
kmeje023a <kmeje023a@student.wethinkcode.co.za>
2023-12-21 02:55:52 +0200

    Added GUI's for Number Guessing game along with Student Grade Calculator

--- NumberGame/src/main/java/org/NumberGame/NumberGameGUI.java
+++ NumberGame/src/main/java/org/NumberGame/NumberGameGUI.java
@@ -1,144 +1,123 @@
-//package org.NumberGame;
-//
-//import javafx.application.Application;
-//import javafx.geometry.Pos;
-//import javafx.scene.Scene;
-//import javafx.scene.control.*;
-//import javafx.scene.layout.VBox;
-//import javafx.stage.Stage;
-//
-//import java.util.Random;
-//
-//public class NumberGameGUI extends Application {
-//    private int numberToGuess;
-//    private int maxAttempts;
-//    private int attempts;
-//    private boolean guessedCorrectly;
-//
-//    public static void main(String[] args) {
-//        launch(args);
-//    }
-//
-//    @Override
-//    public void start(Stage primaryStage) {
-//        primaryStage.setTitle("Number Guessing Game");
-//
-//        VBox vbox = new VBox(10);
-//        vbox.setAlignment(Pos.CENTER);
-//
-//        Label label = new Label("Welcome to the Number Guessing Game!");
-//        Label instructionLabel = new Label("Try to guess the randomly selected number between 1 and 100.");
-//
-//        Button startButton = new Button("Start Game");
-//
-//        startButton.setOnAction(e -> {
-//            startGame();
-//            displayGuessingGame(primaryStage);
-//        });
-//
-//        vbox.getChildren().addAll(label, instructionLabel, startButton);
-//
-//        Scene scene = new Scene(vbox, 400, 200);
-//        primaryStage.setScene(scene);
-//
-//        primaryStage.show();
-//    }
-//
-//    private void startGame() {
-//        Random random = new Random();
-//
-//        // Set difficulty levels
-//        int easyMaxAttempts = 20;
-//        int mediumMaxAttempts = 15;
-//        int hardMaxAttempts = 10;
-//        int veryHardMaxAttempts = 5;
-//
-//        // Set maxAttempts based on the difficulty level chosen
-//        int difficultyChoice = 2;  // Default to Medium
-//        switch (difficultyChoice) {
-//            case 1:
-//                maxAttempts = easyMaxAttempts;
-//                break;
-//            case 2:
-//                maxAttempts = mediumMaxAttempts;
-//                break;
-//            case 3:
-//                maxAttempts = hardMaxAttempts;
-//                break;
-//            case 4:
-//                maxAttempts = veryHardMaxAttempts;
-//                break;
-//            default:
-//                maxAttempts = mediumMaxAttempts;
-//                break;
-//        }
-//
-//        numberToGuess = random.nextInt(100) + 1;
-//        attempts = 0;
-//        guessedCorrectly = false;
-//    }
-//
-//    private void displayGuessingGame(Stage primaryStage) {
-//        VBox vbox = new VBox(10);
-//        vbox.setAlignment(Pos.CENTER);
-//
-//        Label guessLabel = new Label("Enter your guess:");
-//        TextField guessTextField = new TextField();
-//        Button guessButton = new Button("Submit Guess");
-//
-//        guessButton.setOnAction(e -> {
-//            handleGuess(Integer.parseInt(guessTextField.getText()));
-//            if (!guessedCorrectly && attempts < maxAttempts) {
-//                guessTextField.clear();
-//                guessTextField.requestFocus();
-//            } else {
-//                displayResult(primaryStage);
-//            }
-//        });
-//
-//        vbox.getChildren().addAll(guessLabel, guessTextField, guessButton);
-//
-//        Scene scene = new Scene(vbox, 400, 200);
-//        primaryStage.setScene(scene);
-//
-//        primaryStage.show();
-//    }
-//
-//    private void handleGuess(int guess) {
-//        attempts++;
-//
-//        if (guess < numberToGuess) {
-//            System.out.println("Too low! Try again.");
-//        } else if (guess > numberToGuess) {
-//            System.out.println("Too high! Try again.");
-//        } else {
-//            guessedCorrectly = true;
-//        }
-//
-//        if (attempts < maxAttempts) {
-//            // Only ask the user to try again if there are more attempts available
-//            System.out.println("You have " + (maxAttempts - attempts) + " attempts remaining.");
-//        }
-//    }
-//
-//    private void displayResult(Stage primaryStage) {
-//        VBox vbox = new VBox(10);
-//        vbox.setAlignment(Pos.CENTER);
-//
-//        if (guessedCorrectly) {
-//            Label resultLabel = new Label("Congratulations! You've guessed the number " + numberToGuess + " in " + attempts + " attempts.");
-//            vbox.getChildren().add(resultLabel);
-//        } else {
-//            Label resultLabel = new Label("Sorry, you've run out of attempts. The correct number was: " + numberToGuess);
-//            vbox.getChildren().add(resultLabel);
-//        }
-//
-//        Button restartButton = new Button("Play Again");
-//        restartButton.setOnAction(e -> start(primaryStage));
-//
-//        vbox.getChildren().add(restartButton);
-//
-//        Scene scene = new Scene(vbox, 400, 200);
-//        primaryStage.setScene(scene);
-//    }
-//}
+package org.NumberGame;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+public class NumberGameGUI {
+    private JFrame frame;
+    private JTextField guessTextField;
+    private JTextArea resultTextArea;
+
+    private int numberToGuess;
+    private int attempts;
+    private int maxAttempts;
+    private boolean guessedCorrectly;
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            NumberGameGUI gameGUI = new NumberGameGUI();
+            gameGUI.createAndShowGUI();
+        });
+    }
+
+    private void createAndShowGUI() {
+        frame = new JFrame("Number Guessing Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.insets = new Insets(5, 5, 5, 5);
+
+        // Difficulty level choice
+        String[] difficultyOptions = {"Easy", "Medium", "Hard", "Very Hard"};
+        JComboBox<String> difficultyComboBox = new JComboBox<>(difficultyOptions);
+        panel.add(new JLabel("Choose a difficulty level:"), constraints);
+        panel.add(difficultyComboBox, constraints);
+
+        // Start button
+        JButton startButton = new JButton("Start Game");
+        constraints.gridy = 1;
+        panel.add(startButton, constraints);
+
+        // Guess input field
+        guessTextField = new JTextField(10);
+        constraints.gridy = 2;
+        panel.add(new JLabel("Enter your guess:"), constraints);
+        panel.add(guessTextField, constraints);
+
+        // Guess button
+        JButton guessButton = new JButton("Guess");
+        constraints.gridy = 3;
+        panel.add(guessButton, constraints);
+
+        // Result display area
+        resultTextArea = new JTextArea(5, 20);
+        resultTextArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(resultTextArea);
+        constraints.gridy = 4;
+        panel.add(scrollPane, constraints);
+
+        // Initialize the game
+        startButton.addActionListener(e -> startGame(difficultyComboBox.getSelectedIndex()));
+        guessButton.addActionListener(e -> makeGuess());
+
+        frame.add(panel);
+        frame.pack();
+        frame.setLocationRelativeTo(null); // Center the frame
+        frame.setVisible(true);
+    }
+
+    private void startGame(int difficultyIndex) {
+        Random random = new Random();
+        guessedCorrectly = false;
+        attempts = 0;
+
+        switch (difficultyIndex) {
+            case 0:
+                maxAttempts = 20;
+                break;
+            case 1:
+                maxAttempts = 15;
+                break;
+            case 2:
+                maxAttempts = 10;
+                break;
+            case 3:
+                maxAttempts = 5;
+                break;
+            default:
+                maxAttempts = 15; // Default to medium difficulty
+        }
+
+        numberToGuess = random.nextInt(100) + 1;
+        resultTextArea.setText("Welcome to the Number Guessing Game!\n" +
+                "Try to guess the randomly selected number between 1 and 100.\n" +
+                "You have " + maxAttempts + " attempts.");
+    }
+
+    private void makeGuess() {
+        if (!guessedCorrectly && attempts < maxAttempts) {
+            int guess = Integer.parseInt(guessTextField.getText());
+            attempts++;
+
+            if (guess < numberToGuess) {
+                resultTextArea.append("\nToo low! Try again.");
+            } else if (guess > numberToGuess) {
+                resultTextArea.append("\nToo high! Try again.");
+            } else {
+                guessedCorrectly = true;
+                resultTextArea.append("\nCongratulations! You've guessed the number " +
+                        numberToGuess + " in " + attempts + " attempts.");
+            }
+
+            if (attempts < maxAttempts) {
+                resultTextArea.append("\nYou have " + (maxAttempts - attempts) + " attempts remaining.");
+            } else if (!guessedCorrectly) {
+                resultTextArea.append("\nSorry, you've run out of attempts. The correct number was: " + numberToGuess);
+            }
+
+            guessTextField.setText("");
+        }
+    }
+}
--- StudentGradeCalculator/src/main/java/org/StudentGradeCalculator/StudentGradeCalculatorGUI.java
+++ StudentGradeCalculator/src/main/java/org/StudentGradeCalculator/StudentGradeCalculatorGUI.java
@@ -1,83 +1,95 @@
 //package org.StudentGradeCalculator;
 //
-//import javafx.application.Application;
-//import javafx.geometry.Insets;
-//import javafx.geometry.Pos;
-//import javafx.scene.Scene;
-//import javafx.scene.control.*;
-//import javafx.scene.layout.GridPane;
-//import javafx.stage.Stage;
+//import javax.swing.*;
+//import java.awt.*;
 //
-//public class StudentGradeCalculatorGUI extends Application {
+//public class StudentGradeCalculatorGUI {
+//    private JFrame frame;
+//    private JTextField[] subjectFields;
+//    private JTextField[] scoreFields;
+//    private JButton calculateButton;
+//    private JTextArea resultTextArea;
+//
 //    public static void main(String[] args) {
-//        launch(args);
+//        SwingUtilities.invokeLater(() -> {
+//            StudentGradeCalculatorGUI calculatorGUI = new StudentGradeCalculatorGUI();
+//            calculatorGUI.createAndShowGUI();
+//        });
 //    }
 //
-//    @Override
-//    public void start(Stage primaryStage) {
-//        primaryStage.setTitle("Student Grade Calculator");
+//    private void createAndShowGUI() {
+//        frame = new JFrame("Student Grade Calculator");
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 //
-//        GridPane grid = new GridPane();
-//        grid.setAlignment(Pos.CENTER);
-//        grid.setHgap(10);
-//        grid.setVgap(10);
-//        grid.setPadding(new Insets(25, 25, 25, 25));
+//        JPanel panel = new JPanel(new GridBagLayout());
+//        GridBagConstraints constraints = new GridBagConstraints();
+//        constraints.insets = new Insets(5, 5, 5, 5);
 //
-//        TextField[] subjectFields = new TextField[5];
-//        TextField[] scoreFields = new TextField[5];
+//        // Subject and Score input fields
+//        subjectFields = new JTextField[5];
+//        scoreFields = new JTextField[5];
 //
 //        for (int i = 0; i < 5; i++) {
-//            Label subjectLabel = new Label("Subject for Exam " + (i + 1) + ":");
-//            grid.add(subjectLabel, 0, i);
+//            panel.add(new JLabel("Subject for Exam " + (i + 1) + ":"), constraints);
+//            subjectFields[i] = new JTextField(10);
+//            panel.add(subjectFields[i], constraints);
 //
-//            subjectFields[i] = new TextField();
-//            grid.add(subjectFields[i], 1, i);
-//
-//            Label scoreLabel = new Label("Score for Exam " + (i + 1) + ":");
-//            grid.add(scoreLabel, 2, i);
-//
-//            scoreFields[i] = new TextField();
-//            grid.add(scoreFields[i], 3, i);
+//            panel.add(new JLabel("Score for Exam " + (i + 1) + ":"), constraints);
+//            scoreFields[i] = new JTextField(3);
+//            panel.add(scoreFields[i], constraints);
 //        }
 //
-//        Button calculateButton = new Button("Calculate");
-//        grid.add(calculateButton, 1, 5);
+//        // Calculate button
+//        calculateButton = new JButton("Calculate");
+//        calculateButton.addActionListener(e -> calculateGrade());
+//        constraints.gridy = 1;
+//        constraints.gridwidth = 2;
+//        panel.add(calculateButton, constraints);
 //
-//        Label resultLabel = new Label("");
-//        grid.add(resultLabel, 1, 6);
+//        // Result display area
+//        resultTextArea = new JTextArea(5, 30);
+//        resultTextArea.setEditable(false);
+//        JScrollPane scrollPane = new JScrollPane(resultTextArea);
+//        constraints.gridy = 2;
+//        panel.add(scrollPane, constraints);
 //
-//        calculateButton.setOnAction(e -> {
-//            int[] examScores = new int[5];
+//        frame.add(panel);
+//        frame.pack();
+//        frame.setLocationRelativeTo(null); // Center the frame
+//        frame.setVisible(true);
+//    }
 //
-//            for (int i = 0; i < 5; i++) {
+//    private void calculateGrade() {
+//        int[] examScores = new int[5];
+//
+//        for (int i = 0; i < 5; i++) {
+//            try {
 //                examScores[i] = Integer.parseInt(scoreFields[i].getText());
+//            } catch (NumberFormatException e) {
+//                resultTextArea.setText("Invalid input. Please enter valid scores.");
+//                return;
 //            }
+//        }
 //
-//            double average = (examScores[0] + examScores[1] + examScores[2] + examScores[3] + examScores[4]) / 5.0;
-//            double percentage = average * 10;
-//            double roundedPercentage = Math.round(percentage / 100) * 10;
+//        double average = (examScores[0] + examScores[1] + examScores[2] + examScores[3] + examScores[4]) / 5.0;
+//        double percentage = average * 10;
+//        double roundedPercentage = Math.round(percentage / 100) * 10;
 //
-//            resultLabel.setText("The average percentage is: " + roundedPercentage + "%");
+//        resultTextArea.setText("The average percentage is: " + roundedPercentage + "%");
 //
-//            char grade;
-//            if (roundedPercentage >= 90) {
-//                grade = 'A';
-//            } else if (roundedPercentage >= 80) {
-//                grade = 'B';
-//            } else if (roundedPercentage >= 70) {
-//                grade = 'C';
-//            } else if (roundedPercentage >= 60) {
-//                grade = 'D';
-//            } else {
-//                grade = 'F';
-//            }
+//        char grade;
+//        if (roundedPercentage >= 90) {
+//            grade = 'A';
+//        } else if (roundedPercentage >= 80) {
+//            grade = 'B';
+//        } else if (roundedPercentage >= 70) {
+//            grade = 'C';
+//        } else if (roundedPercentage >= 60) {
+//            grade = 'D';
+//        } else {
+//            grade = 'F';
+//        }
 //
-//            resultLabel.setText(resultLabel.getText() + "\n" + "The student's grade is: " + grade);
-//        });
-//
-//        Scene scene = new Scene(grid, 500, 300);
-//        primaryStage.setScene(scene);
-//
-//        primaryStage.show();
+//        resultTextArea.append("\nThe student's grade is: " + grade);
 //    }
 //}
