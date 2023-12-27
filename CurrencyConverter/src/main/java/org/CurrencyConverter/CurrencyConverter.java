0e1bf9c46849365f5d963ff7d38ff041c0335c1e
kmeje023a <kmeje023a@student.wethinkcode.co.za>
2023-12-27 17:51:20 +0200

    Added API for currency converter and updated from hard coded conversion rate.

--- CurrencyConverter/src/main/java/org/CurrencyConverter/CurrencyConverter.java
+++ CurrencyConverter/src/main/java/org/CurrencyConverter/CurrencyConverter.java
@@ -1,66 +1,69 @@
 package org.CurrencyConverter;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class CurrencyConverter {
+
+    static String API_KEY = "38c23884ff23455039726d26";
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         // Exchange rates (as of the current date)
-        double usdToZarRate = 15.25;   // Example exchange rate: 1 USD = 15.25 ZAR
-        double eurToZarRate = 17.80;   // Example exchange rate: 1 EUR = 17.80 ZAR
-        double jpyToZarRate = 0.14;    // Example exchange rate: 1 JPY = 0.14 ZAR
-        double rubToZarRate = 0.21;    // Example exchange rate: 1 RUB = 0.21 ZAR
-        double murToZarRate = 3.75;    // Example exchange rate: 1 MUR = 3.75 ZAR
-        double jmdToZarRate = 0.12;    // Example exchange rate: 1 JMD = 0.12 ZAR
-        double nadToZarRate = 1.00;    // Example exchange rate: 1 NAD = 1.00 ZAR
 
-        System.out.println("Enter the amount in ZAR to convert: ");
+        System.out.println("Enter the amount to convert: ");
         double zarAmount = scanner.nextDouble();
 
-        System.out.println("Enter the country code for the currency conversion (e.g., USD, EUR, JPY, RUB, MUR, JMD, NAD): ");
-        String countryCode = scanner.next().toUpperCase();
+        System.out.println("Enter the base code for the currency conversion (e.g., USD, EUR, JPY, RUB, MUR, JMD, NAD): ");
+        String baseCurrency = scanner.next().toUpperCase();
 
-        double convertedAmount = 0.0;
-        String currencySymbol = "";
+        System.out.println("Enter the target code for the currency conversion (e.g., USD, EUR, JPY, RUB, MUR, JMD, NAD): ");
+        String targetCurrency = scanner.next().toUpperCase();
 
-        switch (countryCode) {
-            case "USD":
-                convertedAmount = Math.round(zarAmount / usdToZarRate / 100) * 100;
-                currencySymbol = "$";
-                break;
-            case "EUR":
-                convertedAmount = Math.round(zarAmount / eurToZarRate / 100) * 100;
-                currencySymbol = "€";
-                break;
-            case "JPY":
-                convertedAmount = Math.round(zarAmount / jpyToZarRate / 100) * 100;
-                currencySymbol = "¥";
-                break;
-            case "RUB":
-                convertedAmount = Math.round(zarAmount / rubToZarRate / 100) * 100;
-                currencySymbol = "₽";
-                break;
-            case "MUR":
-                convertedAmount = Math.round(zarAmount / murToZarRate / 100) * 100;
-                currencySymbol = "₨";
-                break;
-            case "JMD":
-                convertedAmount = Math.round(zarAmount / jmdToZarRate / 100) * 100;
-                currencySymbol = "J$";
-                break;
-            case "NAD":
-                convertedAmount = Math.round(zarAmount / nadToZarRate / 100) * 100;
-                currencySymbol = "N$";
-                break;
-            default:
-                System.out.println("Invalid country code. Please enter a valid code from the specified list.");
-                scanner.close();
-                return;
-        }
+        BigDecimal exchangeRate = fetchExchangeRate(baseCurrency, targetCurrency);
+        double convertedAmount = zarAmount * exchangeRate.doubleValue();
+        String currencySymbol = getCurrencySymbol(targetCurrency);
 
-        System.out.println(zarAmount + " ZAR is approximately " + currencySymbol + " " + convertedAmount + " " + countryCode);
 
+        System.out.println(zarAmount +" "+baseCurrency + " is approximately " + convertedAmount + " " + targetCurrency);
         scanner.close();
+    }
+
+    public static BigDecimal fetchExchangeRate(String baseCurrency, String targetCurrency) {
+        String apiUrl = "https://open.er-api.com/v6/latest/" + baseCurrency;
+        HttpResponse<JsonNode> response = Unirest.get(apiUrl)
+                .header("Accept", "application/json")
+                .queryString("apikey", API_KEY)
+                .asJson();
+
+        JsonObject rates = JsonParser.parseString(response.getBody().toString()).getAsJsonObject().getAsJsonObject("rates");
+        BigDecimal exchangeRate = rates.getAsJsonPrimitive(targetCurrency).getAsBigDecimal();
+
+        System.out.println("Exchange Rate: 1 " + baseCurrency + " = " + exchangeRate + " " + targetCurrency);
+
+        return exchangeRate;
+    }
+
+    public static String getCurrencySymbol(String currencyCode) {
+        // You can expand this method to include more currency symbols
+        switch (currencyCode) {
+            case "USD":
+                return "$";
+            case "EUR":
+                return "€";
+            case "JPY":
+                return "¥";
+            case "ZAR":
+                return "R";
+            default:
+                return currencyCode; // Return code if symbol is not found
+        }
     }
 }
